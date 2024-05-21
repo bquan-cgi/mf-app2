5b26e0b1669afdb41915bfeca4c683ff37ec1029
bquan-cgi <bo.quan@cgi.com>
2024-05-21 22:49:45 +0200

    configure using mf with main

---
+++ src/App2Home.tsx
@@ -0,0 +1,13 @@
+import CounterButton from "mainApp/CounterButton";
+
+export default function App2Home() {
+    return (
+      <div className="app2 p-5 bg-blue-500">
+        <h1>App2 Home</h1>
+        <CounterButton></CounterButton>
+
+        <a href="app2content">go to app2 content</a>
+      </div>
+    );
+  }
+  
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "app2",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App2Home": "./src/App2Home",
+        "./App2Content": "./src/App2Content",
+      },
+      remotes: {
+        mainApp: "http://localhost:5001/assets/remoteEntry.js",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
 })
