a56d68b30260b031fd775c9fedc87b13af8e1d68
bquan-cgi <bo.quan@cgi.com>
2024-05-27 08:08:05 +0200

    add group-ui input

--- src/App2Home.tsx
+++ src/App2Home.tsx
@@ -1,13 +1,18 @@
 import CounterButton from "mainApp/CounterButton";
+import { GroupuiInput, GroupuiButton } from "@group-ui/group-ui-react";
 
 export default function App2Home() {
-    return (
-      <div className="app2 p-5 bg-blue-500">
-        <h1>App2 Home</h1>
-        <CounterButton></CounterButton>
+  return (
+    <div className="app2 p-5 bg-blue-500">
+      <h1>App2 Home</h1>
+      <CounterButton></CounterButton>
 
-        <a href="app2content">go to app2 content</a>
-      </div>
-    );
-  }
-  
+      <GroupuiInput placeholder="Your Name"></GroupuiInput>
+      <GroupuiButton onClick={() => console.log("Clicked")}>
+        Click Me!
+      </GroupuiButton>
+
+      <a href="app2content">go to app2 content</a>
+    </div>
+  );
+}
